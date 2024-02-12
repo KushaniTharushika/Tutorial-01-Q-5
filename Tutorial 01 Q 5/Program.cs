588f22496d4f9db43ad816bb752278d1c6f22145
ASUS <ASUS@KUSHANI>
2024-02-12 19:56:20 +0530

    Add project files.

---
+++ Tutorial 01 Q 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_01_Q_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Multiplication table for {n}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n * i}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
